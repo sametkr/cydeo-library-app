74e2af0ef262ba16d4d2026b1afec00441d565c7
SametK <samet_kara55@hotmail.com>
2022-06-25 14:11:46 +0300

    tools and utilities are created

---
+++ src/test/java/com/library/utilities/Driver.java
@@ -0,0 +1,38 @@
+package com.library.utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Driver {
+
+    private Driver(){
+
+    }
+
+    private static InheritableThreadLocal<WebDriver> driverPool = new InheritableThreadLocal<>();
+
+    public static WebDriver getDriver(){
+        if (driverPool.get() == null){
+            String browserType = ConfigurationProperties.getProperty("browser");
+
+            if (browserType.equals("chrome")){
+                WebDriverManager.chromedriver().setup();
+                driverPool.set(new ChromeDriver());
+                driverPool.get().manage().window().maximize();
+                driverPool.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            }
+
+        }
+        return driverPool.get();
+    }
+
+    public static void closeDriver(){
+        if (driverPool.get() != null){
+            driverPool.get().quit();
+            driverPool.remove();
+        }
+    }
+}
