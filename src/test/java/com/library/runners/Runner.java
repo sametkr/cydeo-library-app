57d76b6a3f46ed5a9690645dca8d4d12ec631bf8
SametK <samet_kara55@hotmail.com>
2022-07-18 15:52:12 +0300

    login

--- src/test/java/com/library/runners/Runner.java
+++ src/test/java/com/library/runners/Runner.java
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
         glue = "com/library/step_definitions",
         publish = false,
         dryRun = false,
-        tags = "@wip"
+        tags = "@smoke"
 )
 public class Runner {
 }
